015866b518cb30ffc49ba6c9a3a834c1d80d1323
thulebonagwala <thulebonagwala@gmail.com>
2025-08-05 07:50:51 +0200

    Setup controler for creating invoice

---
+++ backend/controllers/invoiceController.js
@@ -0,0 +1,53 @@
+const Invoice = require("../models/Invoice");
+
+// @desc    Create new invoice
+// @route   POST /api/invoices
+// @access  Private
+exports.createInvoice = async (req, res) => {
+  try {
+    const user = req.user;
+    const {
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+    } = req.body;
+    
+
+    // subtotal calculation
+    let subtotal = 0;
+    let taxTotal = 0;
+    items.forEach((item) => {
+      subtotal += item.unitPrice * item.quantity;
+      taxTotal += ((item.unitPrice * item.quantity) * (item.taxPercent || 0)) / 100;
+    });
+
+    const total = subtotal + taxTotal;
+
+    const invoice = new Invoice({
+      user,
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+      subtotal,
+      taxTotal,
+      total,
+    });
+
+    await invoice.save();
+    res.status(201).json(invoice);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating invoice", error: error.message });
+  }
+};
